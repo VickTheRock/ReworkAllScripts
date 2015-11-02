410676c6175f0ee894bf561bec0b648695e15f75
VickTheRock <BlecTager@ya.ru>
2015-11-02 11:55:52 +0300

    Update AssemblyInfo.cs

--- EmberSpirit/Properties/AssemblyInfo.cs
+++ EmberSpirit/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("10c4f6eb-1c96-4425-9152-3aafb4e949e0")]
+[assembly: Guid("10c4f6eb-1c96-4425-9152-7aafb4e949e0")]
 
 // Version information for an assembly consists of the following four values:
 //
