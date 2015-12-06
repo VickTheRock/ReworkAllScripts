094455749c6d0bfc48cb6312e2ff569bff327abd
VickTheRock <BlecTager@ya.ru>
2015-12-06 13:26:02 +0300

    Update Program.cs

--- EmberSpirit/Program.cs
+++ EmberSpirit/Program.cs
@@ -14,16 +14,17 @@ namespace EmberSpirit
     internal class Program
     {
 
-        private static Item mom, abyssal, soulring, arcane, shiva, halberd, mjollnir, satanic, dagon, orchid;
+        private static Item mom, abyssal, soulring, arcane, shiva, halberd, mjollnir, satanic, dagon;
         private static Ability Q, W, E, R, D;
         private static bool activated;
         private static bool togleQW;
-        //  private static bool Altcombo;
+
+        private static bool oneULT;
         private static Font txt;
         private static Font not;
         private static Key KeyCombo = Key.D;
         private const Key TogleQW = Key.W;
-        // private const Key Altthisshit = Key.F;
+        private const Key OneUlt = Key.F;
         private static bool loaded;
         static void Main(string[] args)
         {
@@ -121,7 +122,7 @@ namespace EmberSpirit
                 if (shiva == null)
                     shiva = me.FindItem("item_shivas_guard");
 
-                orchid = me.FindItem("item_orchid");
+
 
                 if ( // Q Skill
                         Q != null &&
@@ -163,26 +164,33 @@ namespace EmberSpirit
                 } // E Skill end
 
                 if (//R Skill
-                    R != null &&
-                    R.CanBeCasted() &&
-                    me.CanCast() &&
-                    me.Distance2D(target) <= 1100 &&
-                    Utils.SleepCheck("R")
+                    R != null 
+					&& !oneULT
+					&& R.CanBeCasted()
+					&& me.CanCast()
+					&& me.Distance2D(target) <= 1100
+					&& Utils.SleepCheck("R")
                     )
                 {
                     R.UseAbility(target.Position);
                     Utils.Sleep(90 + Game.Ping, "R");
                 } // R Skill end
-                
-                if (orchid != null 
-                && orchid.CanBeCasted() 
-                && me.Distance2D(target) <= 900 
-                && Utils.SleepCheck("orchid"))
-					{
-						orchid.UseAbility(target);
-						Utils.Sleep(100 + Game.Ping, "orchid");
-					}
-                if (// SoulRing Item 
+
+				if (//R Skill
+					R != null
+					&& oneULT
+					&& R.CanBeCasted()
+					&& me.CanCast()
+					&& me.Distance2D(target) <= 1100
+					&& Utils.SleepCheck("R")
+					)
+				{
+					R.UseAbility(target.Position);
+					Utils.Sleep(7000 + Game.Ping, "R");
+				} // R Skill end
+
+
+				if (// SoulRing Item 
                     soulring != null &&
                     me.Health / me.MaximumHealth <= 0.4 &&
                     me.Mana <= D.ManaCost &&
@@ -285,14 +293,17 @@ namespace EmberSpirit
                 {
                     satanic.UseAbility();
                 } // Satanic Item end
-                var remnant = ObjectMgr.GetEntities<Unit>().Where(unit => unit.Name == "npc_dota_ember_spirit_remnant").ToList();
-                if (remnant == null)
-                {
-                    return;
-                }
+               
+				var remnant = ObjectMgr.GetEntities<Unit>().Where(unit => unit.Name == "npc_dota_ember_spirit_remnant").ToList();
 
-                var goRemnant = target.Distance2D(remnant.First());
-
+				var e = ObjectMgr.GetEntities<Hero>()
+				.Where(x => x.IsAlive && x.Team != me.Team && !x.IsIllusion)
+				.OrderBy(x => x.Position.Distance2D(remnant.OrderBy(y => x.Position.Distance2D(y.Position)).First().Position))
+				.First();
+				if (remnant == null)
+					return;
+					var goRemnant = e.Distance2D(remnant.First());
+				if (remnant !=null)
                 if (//D Skill
                    D.CanBeCasted() &&
                    me.CanCast() &&
@@ -301,215 +312,14 @@ namespace EmberSpirit
                    )
                 {
                     D.UseAbility(target.Position);
-                    Utils.Sleep(350 + Game.Ping, "D");
+                    Utils.Sleep(400 + Game.Ping, "D");
                 }
 
             }
 
         }
 
-        /*     public static void Game_OnUpdateAltCombo(EventArgs args)
-             {
-                 var me = ObjectMgr.LocalHero;
-                 if (!Game.IsInGame || me.ClassID != ClassID.CDOTA_Unit_Hero_EmberSpirit)
-                 {
-                     return;
-                 }
-                 var target = me.ClosestToMouseTarget(2000);
-                 if (target == null)
-                 {
-                     return;
-                 }
-                 if (Altcombo && target.IsAlive && !target.IsInvul())
-
-                 {
-                     //spell
-                     if (Q == null)
-                         Q = me.Spellbook.SpellQ;
-                     if (W == null)
-                         W = me.Spellbook.SpellW;
-                     if (E == null)
-                         E = me.Spellbook.SpellE;
-                     if (R == null)
-                         R = me.Spellbook.SpellR;
-                     if (D == null)
-                         D = me.Spellbook.SpellD;
-                     // item 
-                     if (satanic == null)
-                         satanic = me.FindItem("item_satanic");
-                     if (dagon == null)
-                         dagon = me.GetDagon();
-                     if (halberd == null)
-                         halberd = me.FindItem("item_heavens_halberd");
-                     if (abyssal == null)
-                         abyssal = me.FindItem("item_abyssal_blade");
-                     if (mjollnir == null)
-                         mjollnir = me.FindItem("item_mjollnir");
-                     if (soulring == null)
-                         soulring = me.FindItem("item_soul_ring");
-                     if (arcane == null)
-                         arcane = me.FindItem("item_arcane_boots");
-                     if (mom == null)
-                         mom = me.FindItem("item_mask_of_madness");
-                     if (shiva == null)
-                         shiva = me.FindItem("item_shivas_guard");
-
-                     var wModif = me.Modifiers.Any(y => y.Name == "modifier_ember_spirit_sleight_of_fist_caster");
-                     if ( // Q Skill
-                         Q != null &&
-                         Q.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         me.Distance2D(target) <= 140 &&
-                         Utils.SleepCheck("Q")
-                         )
-                     {
-                         Q.UseAbility();
-                         Utils.Sleep(20 + Game.Ping, "Q");
-                     } // Q Skill end
-                     if ( // W Skill
-                         W != null &&
-                         W.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("W")
-                         )
-                     {
-                         W.UseAbility(target.Position);
-                         Utils.Sleep(200 + Game.Ping, "W");
-                     } // W Skill end
-                     if ( // E Skill
-                         E != null &&
-                         E.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("E") &&
-                         me.Distance2D(target) <= 400
-                         )
-                     {
-                         E.UseAbility();
-                         Utils.Sleep(350 + Game.Ping, "E");
-                     } // E Skill end
-                     if (//R Skill
-                         R != null &&
-                         R.CanBeCasted() &&
-                         wModif           &&
-                         me.CanCast() &&
-                         me.Distance2D(target) <= 400 &&
-                         Utils.SleepCheck("R")
-                         )
-                     {
-                         R.UseAbility(target.Position);
-                         Utils.Sleep(90 + Game.Ping, "R");
-                     } // R Skill end
-                     if (// SoulRing Item 
-                         soulring != null &&
-                         me.Health / me.MaximumHealth <= 0.4 &&
-                         me.Mana <= D.ManaCost &&
-                         soulring.CanBeCasted())
-                     {
-                         soulring.UseAbility();
-                     } // SoulRing Item end
-                     if (// Arcane Boots Item
-                         arcane != null &&
-                         me.Mana <= D.ManaCost &&
-                         arcane.CanBeCasted())
-                     {
-                         arcane.UseAbility();
-                     } // Arcane Boots Item end
-                     if (// Shiva Item
-                         shiva != null &&
-                         shiva.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("shiva") &&
-                         me.Distance2D(target) <= 600
-                         )
-                     {
-                         shiva.UseAbility();
-                         Utils.Sleep(250 + Game.Ping, "shiva");
-                     } // Shiva Item end
-                     if (// MOM
-                         mom != null &&
-                         mom.CanBeCasted() &&
-                         me.CanCast() &&
-                         Utils.SleepCheck("mom") &&
-                         me.Distance2D(target) <= 700
-                         )
-                     {
-                         mom.UseAbility();
-                         Utils.Sleep(250 + Game.Ping, "mom");
-                     } // MOM Item end
-                     if ( // Abyssal Blade
-                         abyssal != null &&
-                         abyssal.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("abyssal") &&
-                         me.Distance2D(target) <= 400
-                         )
-                     {
-                         abyssal.UseAbility(target);
-                         Utils.Sleep(250 + Game.Ping, "abyssal");
-                     } // Abyssal Item end
-                     if ( // Hellbard
-                         halberd != null &&
-                         halberd.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("halberd") &&
-                         me.Distance2D(target) <= 700
-                         )
-                     {
-                         halberd.UseAbility(target);
-                         Utils.Sleep(250 + Game.Ping, "halberd");
-                     } // Hellbard Item end
-                     if ( // Mjollnir
-                         mjollnir != null &&
-                         mjollnir.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("mjollnir") &&
-                         me.Distance2D(target) <= 900
-                        )
-                     {
-                         mjollnir.UseAbility(me);
-                         Utils.Sleep(250 + Game.Ping, "mjollnir");
-                     } // Mjollnir Item end
-                     if (// Dagon
-                         dagon != null &&
-                         dagon.CanBeCasted() &&
-                         me.CanCast() &&
-                         !target.IsMagicImmune() &&
-                         Utils.SleepCheck("dagon")
-                        )
-                     {
-                         dagon.UseAbility(target);
-                         Utils.Sleep(150 + Game.Ping, "dagon");
-                     } // Dagon Item end
-                     if (// Satanic 
-                         satanic != null &&
-                         me.Health / me.MaximumHealth <= 0.3 &&
-                         satanic.CanBeCasted() &&
-                         me.Distance2D(target) <= 400)
-                     {
-                         satanic.UseAbility();
-                     } // Satanic Item end
-                     var remnant = ObjectMgr.GetEntities<Unit>().Where(unit => unit.Name == "npc_dota_ember_spirit_remnant").ToList();
-                     var goRemnant = target.Distance2D(remnant.First());
-                     if (//D Skill
-                        D.CanBeCasted() &&
-                        wModif          &&
-                        me.CanCast()     &&
-                        goRemnant <= 400 &&
-                        Utils.SleepCheck("D")
-                        )
-                     {
-                         D.UseAbility(target.Position);
-                         Utils.Sleep(300 + Game.Ping, "D");
-                     }
-                 }
-             }*/
+        
 
         public static void Game_OnUpdateQW(EventArgs args)
         {
@@ -583,12 +393,11 @@ namespace EmberSpirit
                 else
                     togleQW = false;
             }
-            /* {
-                 if (Game.IsKeyDown(Altthisshit))
-                     Altcombo = true;
-                 else
-                     Altcombo = false;
-             }*/
+            {
+                 if (Game.IsKeyDown(OneUlt) && Utils.SleepCheck("oneULT"))
+                     oneULT = !oneULT;
+					Utils.Sleep(200 + Game.Ping, "oneULT");
+			}
         }
 
         static void CurrentDomain_DomainUnload(object sender, EventArgs e)
@@ -625,14 +434,14 @@ namespace EmberSpirit
             {
                 txt.DrawText(null, "Ember#: go comboQW  [" + TogleQW + "] for toggle combo", 4, 190, Color.Aqua);
             }
-            /*  if (Altcombo)
+            if (oneULT)
               {
-                  txt.DrawText(null, "Ember#: ComboingAltCombo(test)!", 4, 220, Color.Green);
+                  txt.DrawText(null, "Ember#: Use 1 Remnant![" + OneUlt + "]", 4, 220, Color.DarkOrchid);
               }
-              if (!Altcombo)
+            if (!oneULT)
               {
-                  txt.DrawText(null, "Ember#: ALT COMBO(NEED MORE TARGETS)  [" + Altthisshit + "] for toggle combo", 4, 220, Color.DarkOrchid);
-              }*/
+                  txt.DrawText(null, "Ember#: Use All Remnant![" + OneUlt + "]", 4, 220, Color.DarkOrchid);
+              }
 
 
         }
