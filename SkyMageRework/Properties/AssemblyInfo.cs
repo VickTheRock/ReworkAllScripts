6575640e27f62afa1958348175e631a27ec7c1fe
VickTheRock <BlecTager@ya.ru>
2015-12-16 16:23:17 +0300

    Update AssemblyInfo.cs

--- SkyMageRework/Properties/AssemblyInfo.cs
+++ SkyMageRework/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.6.4")]
-[assembly: AssemblyFileVersion("0.0.6.4")]
+[assembly: AssemblyVersion("0.0.7.4")]
+[assembly: AssemblyFileVersion("0.0.7.4")]
