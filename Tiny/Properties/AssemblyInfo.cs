4438ba2dd88f00ccb57ab38f57b076e59fd53c6b
VickTheRock <BlecTager@ya.ru>
2016-03-19 17:29:22 +0300

    gqwefdbewg

--- Tiny/Properties/AssemblyInfo.cs
+++ Tiny/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.6.3")]
-[assembly: AssemblyFileVersion("0.0.6.3")]
+[assembly: AssemblyVersion("0.0.6.44")]
+[assembly: AssemblyFileVersion("0.0.6.44")]
