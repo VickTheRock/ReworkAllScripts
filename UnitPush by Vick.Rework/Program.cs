5eb8942d76d1908a2639d01fe66685301be3ecaa
VickTheRock <BlecTager@ya.ru>
2016-06-19 19:48:29 +0300

    Update Program.cs

--- UnitPush by Vick.Rework/Program.cs
+++ UnitPush by Vick.Rework/Program.cs
@@ -17,47 +17,64 @@ namespace UnitAllPush
     [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
     internal class Program
     {
+        private static readonly List<ParticleEffect> Effects = new List<ParticleEffect>();
+        private const string EffectPath = @"particles\range_display_blue.vpcf";
+        private const string EffectPanicPath = @"particles\range_display_red.vpcf";
 
         private static bool activated;
         private static Font txt;
         private static Font not;
 
-        public static readonly Vector3[] mid =
-           {
-            new Vector3(-5627, -5111, 384),
-            new Vector3(-3973, -3430, 264),
-            new Vector3(-2355, -1870, 256),
-            new Vector3(-622, -306, 128),
-            new Vector3(1277, 842, 256),
-            new Vector3(2867, 2490, 256),
-            new Vector3(4420, 3985, 384),
-            new Vector3(5213, 4717, 384)
+        private static readonly Vector3[] mid =
+        {
+            new Vector3(-5565, -5039, 384),
+            new Vector3(-4436, -3942, 384),
+            new Vector3(-3342, -2809, 256),
+            new Vector3(-2265, -1869, 255),
+            new Vector3(-1083, -887, 256),
+            new Vector3(94, 136, 256),
+            new Vector3(1398, 1045, 256),
+            new Vector3(2522, 2054, 256),
+            new Vector3(3509, 3139, 256),
+            new Vector3(4663, 4079, 384),
+            new Vector3(5233, 4711, 384),
 
         };
-            public static readonly Vector3[] bot =
-            {
-            new Vector3(-2874, -6188, 256),
-            new Vector3(-467, -6305, 384),
-            new Vector3(1892, -6207, 384),
-            new Vector3(4232, -6142, 384),
-            new Vector3(5566, -5869, 384),
-            new Vector3(6166, -3550, 384),
-            new Vector3(6200, -1049, 384),
-            new Vector3(6298, 1210, 290),
-            new Vector3(6271, 3626, 384)
+
+        private static readonly Vector3[] bot =
+        {
+            new Vector3(-4732, -6082, 384),
+            new Vector3(-3248, -6018, 256),
+            new Vector3(-1656, -6057, 256),
+            new Vector3(-157, -6114, 384),
+            new Vector3(1360, -6270, 384),
+            new Vector3(2901, -6142, 384),
+            new Vector3(4398, -5912, 384),
+            new Vector3(5626, -4999, 384),
+            new Vector3(5968, -3497, 384),
+            new Vector3(6183, -2020, 384),
+            new Vector3(6329, -486, 384),
+            new Vector3(6232, 1032, 379),
+            new Vector3(6295, 2602, 384),
+            new Vector3(6368, 3812, 384)
         };
-            public static readonly Vector3[] top =
-            {
-            new Vector3(-6626, -4220, 384),
-            new Vector3(-6576, -1871, 344),
-            new Vector3(-6331, 361, 384),
-            new Vector3(-6309, 2700, 384),
-            new Vector3(-6231, 5075, 384),
-            new Vector3(-4140, 5999, 384),
-            new Vector3(-1769, 6046, 384),
-            new Vector3(488, 5976, 383),
-            new Vector3(2776, 5971, 256),
-            new Vector3(4473, 5823, 386)
+
+        private static readonly Vector3[] top =
+        {
+            new Vector3(-6641, -4192, 384),
+            new Vector3(-6612, -2662, 256),
+            new Vector3(-6443, -1187, 384),
+            new Vector3(-6353, 288, 384),
+            new Vector3(-6254, 1789, 384),
+            new Vector3(-6247, 3348, 384),
+            new Vector3(-6203, 4851, 384),
+            new Vector3(-4948, 5695, 384),
+            new Vector3(-3455, 5748, 384),
+            new Vector3(-1975, 6056, 384),
+            new Vector3(-466, 6005, 383),
+            new Vector3(1039, 5867, 384),
+            new Vector3(2555, 5812, 255),
+            new Vector3(4084, 5802, 384),
         };
 
         private static readonly Menu Menu = new Menu("All Unit's Push", "AllUnit's Push", true);
@@ -100,8 +117,9 @@ namespace UnitAllPush
 
 
 
+        static bool iscreated;
 
-        public static void Game_OnUpdate(EventArgs args)
+        private static void Game_OnUpdate(EventArgs args)
         {
 
             var me = ObjectManager.LocalHero;
@@ -122,98 +140,94 @@ namespace UnitAllPush
                      || creep.ClassID == ClassID.CDOTA_Unit_Broodmother_Spiderling
                      )
                     && creep.IsAlive
+                    && creep.NetworkActivity != NetworkActivity.Move
                     && creep.Team == me.Team
-                    && creep.IsControllable).ToList();
+                    && creep.IsControllable
+                    && creep.IsValid).ToList();
                 if (unit.Count == 0) return;
 
                 Unit fount = ObjectManager.GetEntities<Unit>().FirstOrDefault(x => x.Team == me.Team && x.ClassID == ClassID.CDOTA_Unit_Fountain);
                 List<Unit> tower = ObjectManager.GetEntities<Unit>().Where(x => x.Team != me.Team && x.ClassID == ClassID.CDOTA_BaseNPC_Tower).ToList();
-                var creepsE = ObjectManager.GetEntities<Unit>().Where(creep =>
-                       (creep.ClassID == ClassID.CDOTA_BaseNPC_Creep_Lane
-                       || creep.ClassID == ClassID.CDOTA_BaseNPC_Creep_Siege
-                       || creep.ClassID == ClassID.CDOTA_BaseNPC_Creep_Neutral
-                       || creep.ClassID == ClassID.CDOTA_BaseNPC_Creep) &&
-                      creep.IsAlive && creep.Team != me.Team && creep.IsVisible && creep.IsSpawned).ToList();
-                var creepsA = ObjectManager.GetEntities<Unit>().Where(creep =>
-                       (creep.ClassID == ClassID.CDOTA_BaseNPC_Creep_Lane
-                       || creep.ClassID == ClassID.CDOTA_BaseNPC_Creep_Siege
-                       || creep.ClassID == ClassID.CDOTA_BaseNPC_Creep_Neutral
-                       || creep.ClassID == ClassID.CDOTA_BaseNPC_Creep) &&
-                      creep.IsAlive && creep.Team == me.Team && creep.IsVisible && creep.IsSpawned).ToList();
 
                 for (int i = 0; i < unit.Count(); ++i)
                 {
+                    Vector3 Mid = GetClosestToVector(mid, unit[i]);
+                    Vector3 Top = GetClosestToVector(top, unit[i]);
+                    Vector3 Bot = GetClosestToVector(bot, unit[i]);
                     if (me.Distance2D(unit[i]) <= 600) return;
                     var v =
                             ObjectManager.GetEntities<Hero>()
                                 .Where(x => x.Team != me.Team && x.IsAlive && x.IsVisible && !x.IsIllusion)
                                 .ToList();
-                    if (v.Count(x => x.Distance2D(unit[i]) <= 900) >= 1
-                    && unit[i].CanMove()
-                    && Utils.SleepCheck(unit[i].Handle.ToString()))
+                   
+                    if (Mid.Distance2D(unit[i]) <= 1700)
                     {
-                        unit[i].Move(fount.Position);
-                        Utils.Sleep(1500, unit[i].Handle.ToString());
-                    }
-                    if (unit[i].NetworkActivity == NetworkActivity.Move) return;
-                    Vector3 Mid = GetClosestToVector(mid, unit[i]);
-
-                    if (Mid.Distance2D(unit[i]) <= 2700 && Utils.SleepCheck(unit[i].Handle.ToString()))
-                    {
-                        for (int z = 0; z < mid.Count(); ++z)
-                        {
-                            for (int j = 0; j < mid.Count(); ++j)
+                         for (int x = 0; x < mid.Count(); ++x)
                             {
-                                var a = mid[j];
-                                var b = mid[z];
-                                if (a.Distance2D(b) <= 2700 &&
-                                    a.Distance2D(fount) < b.Distance2D(fount) && unit[i].Distance2D(b) <= 2700)
+                                var b = mid[x];
+                                if (
+                                    unit[i].Distance2D(fount)+170 < b.Distance2D(fount) && unit[i].Distance2D(b) <= 1700 && Utils.SleepCheck(unit[i].Handle.ToString()))
                                 {
-                                    unit[i].Attack(b);
+                                    unit[i].Attack(b); Utils.Sleep(400, unit[i].Handle.ToString());
                                 }
                             }
-                        }
-                        Utils.Sleep(1500, unit[i].Handle.ToString());
                     }
-                    Vector3 Bot = GetClosestToVector(bot, unit[i]);
-
-                    if (Bot.Distance2D(unit[i]) <= 2700 && Utils.SleepCheck(unit[i].Handle.ToString()))
+                    if (Bot.Distance2D(unit[i]) <= 1700)
                     {
-                        for (int z = 0; z < bot.Count(); ++z)
-                        {
-                            for (int j = 0; j < bot.Count(); ++j)
+                         for (int x = 0; x < bot.Count(); ++x)
                             {
-                                var a = bot[j];
-                                var b = bot[z];
-                                if (a.Distance2D(b) <= 2700 &&
-                                    a.Distance2D(fount) < b.Distance2D(fount) && unit[i].Distance2D(b) <= 2700)
+                                var b = bot[x];
+                                if (
+                                    unit[i].Distance2D(fount) + 170 < b.Distance2D(fount) && unit[i].Distance2D(b) <= 1700 && Utils.SleepCheck(unit[i].Handle.ToString()))
                                 {
-                                    unit[i].Attack(b);
+                                    unit[i].Attack(b); Utils.Sleep(400, unit[i].Handle.ToString());
                                 }
                             }
-                        }
-                        Utils.Sleep(1500, unit[i].Handle.ToString());
+                        
                     }
-                    Vector3 Top = GetClosestToVector(top, unit[i]);
-
-                    if (Top.Distance2D(unit[i]) <= 2700 && Utils.SleepCheck(unit[i].Handle.ToString()))
+                    if (Top.Distance2D(unit[i]) <= 1700)
                     {
-                        for (int z = 0; z < top.Count(); ++z)
-                        {
-                            for (int j = 0; j < top.Count(); ++j)
+                        for (int x = 0; x < top.Count(); ++x)
                             {
-                                var a = top[j];
-                                var b = top[z];
-                                if (a.Distance2D(b) <= 2700 &&
-                                    a.Distance2D(fount) < b.Distance2D(fount) && unit[i].Distance2D(b) <= 2700)
+                                var b = top[x];
+                                if (
+                                    unit[i].Distance2D(fount) + 170 < b.Distance2D(fount) && unit[i].Distance2D(b) <= 1700 && Utils.SleepCheck(unit[i].Handle.ToString()))
                                 {
-                                    unit[i].Attack(b);
+                                    unit[i].Attack(b); Utils.Sleep(400, unit[i].Handle.ToString());
                                 }
                             }
-                        }
-                        Utils.Sleep(1500, unit[i].Handle.ToString());
+                        
                     }
                 }
+                /*
+                for (int i = 0; i < mid.Count(); ++i)
+                {
+                    if (!iscreated)
+                    {
+                        ParticleEffect effect = new ParticleEffect(EffectPanicPath, mid[i]);
+                        effect.SetControlPoint(1, new Vector3(100, 0, 0));
+                        Effects.Add(effect);
+                    }
+                }
+                for (int i = 0; i < bot.Count(); ++i)
+                {
+                    if (!iscreated)
+                    {
+                        ParticleEffect effect = new ParticleEffect(EffectPanicPath, bot[i]);
+                        effect.SetControlPoint(1, new Vector3(100, 0, 0));
+                        Effects.Add(effect);
+                    }
+                }
+                for (int i = 0; i < top.Count(); ++i)
+                {
+                    if (!iscreated)
+                    {
+                        ParticleEffect effect = new ParticleEffect(EffectPanicPath, top[i]);
+                        effect.SetControlPoint(1, new Vector3(100, 0, 0));
+                        Effects.Add(effect);
+                    }
+                }
+                iscreated = true;*/
             }
         }
 
